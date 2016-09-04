680e78eb740d49e4187c6cd5b9c38e2d79069491
Adrian Rossing <adriangrossing@gmail.com>
2016-09-04 14:06:54 -0500

    top absence

--- src/app/components/dashboard/top-absences/top-absences.ts
+++ src/app/components/dashboard/top-absences/top-absences.ts
@@ -1,8 +1,19 @@
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {AbsenceService} from '../../../models/absence/absence.service';
+import {Absence} from '../../../models/DTOs/absence';
 
 @Component({
   selector: 'top-absences',
-  template: require('./top-absences.html')
+  template: require('./top-absences.html'),
+  directives: [ROUTER_DIRECTIVES]
 })
 export class TopAbsences {
+  absences: Absence[];
+  addingNewAbsence: boolean;
+  constructor(private absenceService: AbsenceService) {
+  	//TODO: Get Logged In User!!!!
+    this.addingNewAbsence = false;
+  	this.absences = this.absenceService.getAllAbsences();//.getAbsencesByUserID(10)
+  }
 }
